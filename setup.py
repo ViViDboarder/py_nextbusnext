38055156c41776d9c9765e994db239534bf9c946
ViViDboarder <ViViDboarder@gmail.com>
2023-09-26 12:04:07 -0700

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="py_nextbusnext",
-    version="0.1.5",
+    version="1.0.0",
     author="ViViDboarder",
     description="Minimalistic Python client for the NextBus public API for real-time transit "
     "arrival data",
