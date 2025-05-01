a4dffb6a2aa70bc43ebedfa2196be7b5b85e2d99
ViViDboarder <ViViDboarder@gmail.com>
2025-05-01 16:06:02 -0700

    Bump version to retrigger build

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="py_nextbusnext",
-    version="2.1.0",
+    version="2.1.1",
     author="ViViDboarder",
     description="Minimalistic Python client for the NextBus public API for real-time transit "
     "arrival data",
